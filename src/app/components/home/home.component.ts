8f7b3de38e95d0ac8f16efcf327f31289ed68a35
alexander <alexander.van1dalen@gmail.com>
2021-11-27 02:34:11 +0100

    Mocked front working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Person } from 'src/app/models/person.model';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  editMode: boolean = false;
+  toEdit: number = 0;
+  toDelete: number = 0;
 
-  constructor() { }
+  personForm!: FormGroup;
 
-  ngOnInit(): void {
+  formTemplate: any = {
+    lastname: ['', Validators.required],
+    firstname: ['', Validators.required],
+    student: false
   }
 
+  people: Person[] = [
+    { id: 1, firstname: "Alexander", lastname: "van Dalen", student: false },
+    { id: 2, firstname: "Guillaume", lastname: "Varoquaux", student: false },
+    { id: 3, firstname: "Jean", lastname: "Dupont", student: true },
+  ]
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.personForm = this.formBuilder.group(this.formTemplate);
+  }
+
+  onCreatePerson() {
+    let person: Person = this.personForm.value;
+    if (this.editMode) {
+      // update
+      this.people.splice(this.toEdit, 1, person);
+    } else  {
+      // create
+      this.people.push(person);
+    }
+    this.abortEdit();
+  }
+
+  onEditPerson(person: Person, id: number) {
+    this.editMode = true;
+    this.toEdit = id;
+
+    this.personForm.get("id")?.setValue(person.id);
+    this.personForm.get("lastname")?.setValue(person.lastname);
+    this.personForm.get("firstname")?.setValue(person.firstname);
+    this.personForm.get("student")?.setValue(person.student);
+  }
+
+  onRemovePerson(id: number) {
+      this.toDelete = id;
+  }
+
+  onConfirmRemove() {
+    this.people.splice(this.toDelete, 1);
+    this.toDelete = 0;
+  }
+
+  abortEdit() {
+    this.personForm.reset();
+    this.editMode = false;
+    this.toEdit = 0;
+  }
 }
--- src/app/models/person.model.ts
+++ src/app/models/person.model.ts
@@ -1,6 +1,7 @@
 export class Person {
 
   constructor(
+    public id: number,
     public firstname: string,
     public lastname: string,
     public student: boolean
